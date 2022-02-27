a44aab8082a70f0dd15e312a854367639d340259
Jesus David Ortiz Arevalo <davinchi89@hotmail.com>
2022-02-27 12:49:14 -0500

    Desarrollo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'binding-proyect';
+
+  name!: string;
+  edad!: number;
+  public mostrar:boolean = false; 
+
+  Opersona: {
+    nombre: string,
+    edad: number,
+  }
+
+  persona: any=[];
+
+  constructor() {
+    this.Opersona = { nombre:'', edad:0}
+  }
+    
+  /**
+ * agregar
+ */
+public agregar() {
+  this.persona.push(
+    { nombre: this.Opersona.nombre }
+  )
 }
+  
+
+  
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BindingComponent } from './binding/binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
